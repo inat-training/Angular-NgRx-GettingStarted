95d916336ce3e5395c74996024bee45b67939d93
Perrence Muzavazi <mperrence@gmail.com>
2021-06-22 21:03:52 +0200

    add ngrx store devtools

--- APM-Demo0/src/app/app.module.ts
+++ APM-Demo0/src/app/app.module.ts
@@ -1,22 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 // Imports for loading & configuring the in-memory web api
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { ProductData } from './products/product-data';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {ProductData} from './products/product-data';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { ShellComponent } from './home/shell.component';
-import { MenuComponent } from './home/menu.component';
-import { WelcomeComponent } from './home/welcome.component';
-import { PageNotFoundComponent } from './home/page-not-found.component';
+import {AppComponent} from './app.component';
+import {ShellComponent} from './home/shell.component';
+import {MenuComponent} from './home/menu.component';
+import {WelcomeComponent} from './home/welcome.component';
+import {PageNotFoundComponent} from './home/page-not-found.component';
 
 /* Feature Modules */
-import { UserModule } from './user/user.module';
-import { StoreModule } from '@ngrx/store';
+import {UserModule} from './user/user.module';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
 
 @NgModule({
   imports: [
@@ -25,7 +27,12 @@ import { StoreModule } from '@ngrx/store';
     HttpClientInMemoryWebApiModule.forRoot(ProductData),
     UserModule,
     AppRoutingModule,
-    StoreModule. forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      name: 'APM Demo App DevTools',
+      maxAge: 25,
+      logOnly: environment.production
+    })
   ],
   declarations: [
     AppComponent,

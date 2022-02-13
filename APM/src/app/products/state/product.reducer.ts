f8334aae01222df909224a389ff7995b2bb14d0a
Perrence Muzavazi <7643707+mperrence@users.noreply.github.com>
2022-02-13 11:34:47 +0200

    add initial ngrx impl

---
+++ APM/src/app/products/state/product.reducer.ts
@@ -0,0 +1,15 @@
+import {
+  createAction,
+  createReducer,
+  on
+} from "@ngrx/store";
+
+export const productReducer = createReducer(
+  { showProductCode: true},
+  on(createAction('[Product] Toggle product Code'), state => {
+    return {
+      ...state,
+      showProductCode: !state.showProductCode
+    }
+  })
+);
